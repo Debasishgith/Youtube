9f9b427d40c482afc102337dee3f7419dfeccb0e
Debasish Borah <debasishborah1998@gmail.com>
2026-09-16 18:59:18 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-// Serve uploaded files with proper CORS headers for video streaming
+
 app.use(
   "/uploads",
   (req, res, next) => {
