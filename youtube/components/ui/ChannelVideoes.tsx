c7887b7a2a58c2eac0f5e301b98a04201f5b6fc9
Debasish Borah <debasishborah1998@gmail.com>
2026-09-16 19:54:12 +0530

    fixed styling

--- youtube/components/ui/ChannelVideoes.tsx
+++ youtube/components/ui/ChannelVideoes.tsx
@@ -10,8 +10,8 @@ export default function ChannelVideos({ videos }: any) {
   }
 
   return (
-    <div>
-      <h2 className="text-xl font-semibold mb-4 p-6">Videos</h2>
+    <div className="p-6">
+      <h2 className="text-xl font-semibold mb-4 ">Videos</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {videos.map((video: any) => (
           <VideoCard key={video._id} video={video} />
