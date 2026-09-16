895da9d4577ee471cd02013458160745fa524530
Debasish Borah <debasishborah1998@gmail.com>
2026-09-16 11:58:16 +0530

    change in cors value again

--- server/index.js
+++ server/index.js
@@ -14,7 +14,13 @@ import path from "path";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://youtube-chi-five-44.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 // Serve uploaded files with proper CORS headers for video streaming
